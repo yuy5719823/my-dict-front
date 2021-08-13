b0ede8087376d2b5b908f4084aeb0ce21e60e16b
yuy5719823 <youya_827@yahoo.co.jp>
2021-08-13 18:34:31 +0900

    [Add]ログインページの作成

---
+++ src/components/atoms/button/PrimaryButton.tsx
@@ -0,0 +1,18 @@
+import { VFC, memo, ReactNode } from 'react';
+import { Button } from '@chakra-ui/react';
+
+type Props = {
+  children: ReactNode;
+}
+
+export const PrimaryButton: VFC<Props> = memo((props) => {
+
+  const {children} = props;
+
+  return(
+    <Button width="100%" backgroundColor="teal.400" color="white" _hover={{opacity: 0.8}}>
+      {children}
+    </Button>
+    );
+
+});
--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,8 +1,26 @@
 import { VFC, memo } from 'react';
+import { Input, Flex, Box, Heading, Divider, Link, Stack } from '@chakra-ui/react';
+
+import { PrimaryButton } from '../atoms/button/PrimaryButton';
 
 export const Login: VFC = memo(() => {
   return(
-    <p>ログインページです</p>
+    <Flex align="center" justify="center" height="100vh" flexDirection="column">
+      <Box bg="white" w="sm" p={4} borderRadius={8} shadow="md">
+        <Heading as="h1" size="lg" textAlign="center" fontWeight="light" color="gray.600">MyDictionary</Heading>
+        <Divider　m={2} />
+        <Stack spacing={4} py={3} px={8} >
+          <Input type="email" placeholder="メールアドレスを入力" />
+          <Input type="password" placeholder="パスワードを入力" />
+          <PrimaryButton>ログイン</PrimaryButton>
+        </Stack>
+      </Box>
+    <Box py={8}>
+      <Link color="gray.500" borderBottom="solid 2px white" _hover={{ backgroundColor: "gray.50" }} p={1} borderRadius={8}>
+        アカウントの作成
+      </Link>
+    </Box>
+    </Flex>
   );
 
 });
