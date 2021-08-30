3dbbd4131bec672e236322c992879cd4d1374fe3
yuy5719823 <youya_827@yahoo.co.jp>
2021-08-30 16:10:50 +0900

    [Edit]404ページ

--- src/components/pages/Page404.tsx
+++ src/components/pages/Page404.tsx
@@ -1,8 +1,16 @@
+import { Box, Heading, Link } from '@chakra-ui/react';
 import { VFC, memo } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export const Page404: VFC = memo(() => {
+
+  const history = useHistory();
+
   return(
-    <p>404ページです</p>
-  );
+    <Box textAlign="center" my="120px">
+      <Heading as="h1" p={4}>ページが見つかりません</Heading>
+      <Link onClick={() => history.push("/home")}>TOPページに戻る</Link>
+    </Box>
+    );
 
 });
