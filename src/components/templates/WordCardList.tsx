ebe6236c5d7f35e8aef241d0cce1b23832aceec4
yuy5719823 <youya_827@yahoo.co.jp>
2021-09-13 23:38:28 +0900

    [Add]ソート機能

--- src/components/templates/WordCardList.tsx
+++ src/components/templates/WordCardList.tsx
@@ -1,11 +1,12 @@
 import { VFC, memo, useEffect, useCallback, useState } from 'react';
-import { Center, Spinner, Wrap, WrapItem, Box, useDisclosure } from '@chakra-ui/react';
+import { Center, Spinner, Wrap, WrapItem, Box, useDisclosure, Button, Flex } from '@chakra-ui/react';
 
 import { WordCard } from '../organisms/word/WordCard';
 import { useWordList } from '../../hooks/useWordList';
 import { WordDetailModal } from '../organisms/word/WordDetailModal';
 import { useSetWord } from '../../hooks/useSelectWord';
 import { AddWordModal } from '../organisms/word/AddWordModal';
+import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 
 type Props = {
   isOpenAdd?: boolean;
@@ -32,6 +33,49 @@ export const WordCardList:VFC<Props> = memo((props) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect( () => fetchWordList({mode}) ,[update] );
 
+  let sortedWordList = wordList;
+  const [ sort, setSort ] = useState<string>("");
+  const [ sortSwitchName, setSortSwitchName ] = useState<boolean>(false);
+  const [ sortSwitchCreate, setSortSwitchCreate ] = useState<boolean>(true);
+
+  const onClickSortName = () => {
+    if(sortSwitchName){
+      setSort("name");
+    }else{
+      setSort("nameDesc");
+    }
+    setSortSwitchName(!sortSwitchName);
+    if(sortSwitchCreate){
+      setSortSwitchCreate(false);
+    };
+  };
+  const onClickSortCreate = () => {
+    if(sortSwitchCreate){
+      setSort("createdAtDesc")
+    }else{
+      setSort("createdAt");
+    }
+    setSortSwitchCreate(!sortSwitchCreate);
+    if(sortSwitchName){
+      setSortSwitchName(false);
+    }
+  };
+
+  switch(sort){
+    case "createdAt":
+      sortedWordList = wordList.sort((a,b) => b.created_at.localeCompare(a.created_at));
+      break;
+    case "createdAtDesc":
+      sortedWordList = wordList.sort((a,b) => a.created_at.localeCompare(b.created_at));
+      break;
+    case "name":
+      sortedWordList = wordList.sort((a,b) => a.word.localeCompare(b.word));
+      break;
+    case "nameDesc":
+      sortedWordList = wordList.sort((a,b) => b.word.localeCompare(a.word));
+      break;
+  }
+
   return(
     <>
       {loading === true ? (
@@ -40,6 +84,10 @@ export const WordCardList:VFC<Props> = memo((props) => {
         </Center>
       ) : (
         <Wrap p={{base: 4, md: 8 }} justify="center" direction="column" spacing="30px" >
+          <Flex justifyContent="center" >
+            <Button as="a" _hover={{cursor:"pointer"}} onClick={onClickSortName}>名前順{sortSwitchName? <ChevronUpIcon/> : <ChevronDownIcon/>}</Button>
+            <Button as="a" _hover={{cursor:"pointer"}} onClick={onClickSortCreate}>作成順{sortSwitchCreate? <ChevronUpIcon/> : <ChevronDownIcon/>}</Button>
+          </Flex>
           {wordList.length === 0 ? (
             <WrapItem  h="90vh">
               <Box m="auto">
@@ -48,7 +96,7 @@ export const WordCardList:VFC<Props> = memo((props) => {
             </WrapItem>
           ) : (
             <>
-            {wordList.map( (word) => (
+            {sortedWordList.map( (word) => (
               <WrapItem key={word.id}>
                 <WordCard id={word.id} title={word.word} wordMemo={word.memo} onClick={onClickWord} />
               </WrapItem>
